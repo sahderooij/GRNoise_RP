46aedbb7643659997cff07789f96e85121684000
sahderooij <62094930+sahderooij@users.noreply.github.com>
2021-03-08 15:47:11 +0100

    Submitted_v1

--- python_code/kidata/calc.py
+++ python_code/kidata/calc.py
@@ -1,5 +1,5 @@
 import numpy as np
-import warnings 
+import warnings
 import matplotlib.pyplot as plt
 
 from scipy import integrate
@@ -16,20 +16,20 @@ from kidata.plot import _selectPread
 # NOTE: all units are in 'micro': µeV, µm, µs etc.
 
 def ak(S21data, lbd0=0.092, N0=1.72e4, kbTD=37312.0,plot=False,reterr=False,method='df'):
-    '''Calculates the kinetic induction fraction, based on Goa2008, PhD Thesis. 
+    '''Calculates the kinetic induction fraction, based on Goa2008, PhD Thesis.
     Arguments:
-    S21data -- the content of the .csv from the S21-analysis. 
+    S21data -- the content of the .csv from the S21-analysis.
     lbd0 -- penetration depth at T=0 in µm (default: 0.092, Al)
     N0 -- Density of states in µeV^{-1}µm^{-3} (default: 1.72e4, Al)
     kbTD -- Debye Energy in µeV (default 37312.0, Al)
     plot -- boolean to plot the fit over temperature.
     reterr -- boolean to return fitting error.
     method -- either df or Qi, which is fitted linearly over temperature.
-    
+
     Returns:
-    ak 
+    ak
     optionally: the error as well.'''
-    
+
     # Extract relevant data
     hw = S21data[:, 5] * 2 * np.pi * 0.6582 * 1e-9 #µeV
     kbT = S21data[:, 1] * 86.17  #µeV
@@ -39,21 +39,21 @@ def ak(S21data, lbd0=0.092, N0=1.72e4, kbTD=37312.0,plot=False,reterr=False,meth
     d = S21data[0, 25]
     kbTc = S21data[0,21] * 86.17
     D0 = 1.76 * kbTc
-    
+
     # define y to fit:
     if method == 'df':
         y = (hw - hw0) / hw0
     elif method == 'Qi':
         y = 1/S21data[:,4] - 1/S21data[0,4]
-    
-    
-    
+
+
+
     # Mask the double measured temperatures, and only fit from 250 mK
     mask1 = np.zeros(len(y), dtype="bool")
     mask1[np.unique(np.round(S21data[:, 1], decimals=2),
                     return_index=True)[1]] = True
     mask = np.logical_and(mask1, (kbT >= .25 * 86.17))
-    
+
     if mask.sum() > 3:
         y = y[mask]
     else:
@@ -61,7 +61,7 @@ def ak(S21data, lbd0=0.092, N0=1.72e4, kbTD=37312.0,plot=False,reterr=False,meth
         y = y[mask1][-10:]
 
 
-    
+
     # define x to fit:
     x = np.zeros(len(y))
     i = 0
@@ -74,7 +74,7 @@ def ak(S21data, lbd0=0.092, N0=1.72e4, kbTD=37312.0,plot=False,reterr=False,meth
         elif method == 'Qi':
             x[i] = (s[0] - s0[0]) / s0[1] * beta(lbd0, d, D_0, D0, kbTi)/2
         i += 1
-    
+
     #do the fit:
     fit = curve_fit(lambda t, ak: ak * t, x, y)
     if plot:
@@ -105,12 +105,12 @@ def tau(freq, SPR,startf=None,stopf=None,decades=3,minfreq=1e2,plot=False,retfnl
     minfreq -- adjusts minimum frequency for startf, if startf is None
     plot -- boolean to show the plot
     retfnl -- boolean to return the noise level as well.
-    
+
     Returns:
     tau -- lifetime in µs
     tauerr -- error in tau
     optionally: noise level (non-dB) and  error in noise level.'''
-    
+
     #Filter nan-values
     freq = freq[SPR!=-140]
     SPR = SPR[SPR!=-140]
@@ -126,7 +126,7 @@ def tau(freq, SPR,startf=None,stopf=None,decades=3,minfreq=1e2,plot=False,retfnl
             stopf = 25e4
     if startf is None:
         startf = max(10**(np.log10(stopf)-decades),minfreq)
-    
+
     # fitting a Lorentzian
     fitmask = np.logical_and(freq >= startf, freq <= stopf)
     fitfreq = freq[fitmask]
@@ -137,13 +137,13 @@ def tau(freq, SPR,startf=None,stopf=None,decades=3,minfreq=1e2,plot=False,retfnl
         N = np.nan
         Nerr = np.nan
     else:
-        fitPSD = 10**(np.real(SPR[fitmask]-SPR.max())/10) 
+        fitPSD = 10**(np.real(SPR[fitmask]-SPR.max())/10)
         #notice the normalization for robust fitting
 
         def Lorspec(f, t, N):
             SN = 4 * N * t / (1 + (2 * np.pi * f * t) ** 2)
             return SN
-        
+
         try:
             fit = curve_fit(Lorspec, fitfreq, fitPSD,
                             bounds=([0, 0], [np.inf, np.inf]),
@@ -162,12 +162,12 @@ def tau(freq, SPR,startf=None,stopf=None,decades=3,minfreq=1e2,plot=False,retfnl
             plt.plot(fitfreq, 10*np.log10(Lorspec(fitfreq,tau*1e-6,N)))
         plt.xscale("log")
         plt.show(); plt.close()
-        
+
     if retfnl:
         return tau,tauerr,4*N*tau*1e-6,np.sqrt((4e-6*N*tauerr)**2 + (4e-6*Nerr*tau)**2)
     else:
         return tau,tauerr
-    
+
 def tau_pulse(pulse,tfit=(10,1e3),reterr=False,plot=False):
     '''Calculates lifetime from a exponential fit to a pulse.
     Arguments:
@@ -175,18 +175,18 @@ def tau_pulse(pulse,tfit=(10,1e3),reterr=False,plot=False):
     tfit -- tuple to specify the fitting window, default is (10,1e3)
     reterr -- boolean to return error
     plot -- boolean to plot the fit
-    
+
     Returns:
     tau -- in µs
     optionally the fitting error'''
-    t = (np.arange(len(pulse)) - 500) 
+    t = (np.arange(len(pulse)) - 500)
     fitmask = np.logical_and(t > tfit[0], t < tfit[1])
     t2 = t[fitmask]
     peak2 = pulse[fitmask]
     fit = curve_fit(
         lambda x, a, b: b * np.exp(-x / a), t2, peak2, p0=(0.5e3, peak2[0])
     )
-    
+
     if plot:
         plt.figure()
         plt.plot(t, pulse)
@@ -195,9 +195,9 @@ def tau_pulse(pulse,tfit=(10,1e3),reterr=False,plot=False):
         plt.show();plt.close()
     if reterr:
         return fit[0][0],np.sqrt(fit[1][0,0])
-    else: 
+    else:
         return fit[0][0]
-    
+
 def tesc(Chipnum,KIDnum,Pread='max',
               minTemp=200,maxTemp=400,taunonkaplan=2e2,taures=1e1,relerrthrs=.2,
               pltfit=False,pltkaplan=False,reterr=False,
@@ -211,16 +211,15 @@ def tesc(Chipnum,KIDnum,Pread='max',
     Uses data at Pread (default max), and temperatures between minTemp,maxTemp
     (default (300,400)). Only lifetimes between taunonkaplan and taures, and with
     a relative error threshold of relerrthrs are considered.
-    The remaining lifetimes, tesc is calculated and averaged. The error (optional return) 
-    is the variance of the remaining lifetimes. If this fails, defaulttesc is returned.
-    TODO: replace this with a true fitting procedure.'''
-    
+    The remaining lifetimes, tesc is calculated and averaged. The error (optional return)
+    is the variance of the remaining lifetimes. If this fails, defaulttesc is returned.'''
+
     TDparam = io.get_grTDparam(Chipnum)
     Pread = _selectPread(Pread,io.get_grPread(TDparam,KIDnum))[0]
 
     kbTc = io.get_S21data(Chipnum,KIDnum,
                        io.get_S21Pread(Chipnum,KIDnum)[0])[0,21]*kb
-    
+
     Temp = io.get_grTemp(TDparam,KIDnum,Pread)
     Temp = Temp[np.logical_and(Temp<maxTemp,Temp>minTemp)]
     tescar,tescarerr,tqpstar,tqpstarerr = np.zeros((4,len(Temp)))
@@ -230,7 +229,7 @@ def tesc(Chipnum,KIDnum,Pread='max',
                 Chipnum,KIDnum,Pread,Temp[i]))
         freq,SPR = io.get_grdata(TDparam,KIDnum,Pread,Temp[i])
         tqpstar[i],tqpstarerr[i] = tau(freq,SPR,plot=pltfit)
-        
+
         if tqpstarerr[i]/tqpstar[i] > relerrthrs or \
         (tqpstar[i] > taunonkaplan or tqpstar[i] < taures):
             tescar[i] = np.nan
@@ -239,10 +238,10 @@ def tesc(Chipnum,KIDnum,Pread='max',
                              t0,tpb,N0,kbTc,kbTD)
             tescarerr[i] = np.abs(kidcalc.tesc(kb*Temp[i]*1e-3,tqpstarerr[i],
                              t0,tpb,N0,kbTc,kbTD)+tpb)
-            
+
     if tescar[~np.isnan(tescar)].size > 0:
         tesc1 = np.mean(tescar[~np.isnan(tescar)])
-        tescerr = np.sqrt(np.std(tescar[~np.isnan(tescar)])**2 + 
+        tescerr = np.sqrt(np.std(tescar[~np.isnan(tescar)])**2 +
                           ((tescarerr[~np.isnan(tescar)]/(~np.isnan(tescar)).sum())**2).sum())
     else:
         tesc1 = np.nan
@@ -262,7 +261,7 @@ def tesc(Chipnum,KIDnum,Pread='max',
                        Temp[~np.isnan(tqpstar)].max(),100)
         except ValueError:
             T = np.linspace(minTemp,maxTemp,100)
-        taukaplan = kidcalc.tau_kaplan(T*1e-3,tesc=tesc1,kbTc=kbTc)  
+        taukaplan = kidcalc.tau_kaplan(T*1e-3,tesc=tesc1,kbTc=kbTc)
         plt.plot(T,taukaplan)
         plt.yscale('log')
         plt.ylim(None,1e4)
@@ -286,10 +285,10 @@ def get_tescdict(Chipnum,Pread='max'):
 
 def NqpfromQi(S21data,uselowtempapprox=True,lbd0=0.092,kb=86.17,N0=1.72e4,kbTD=37312.0):
     '''Calculates the number of quasiparticles from the measured temperature dependence of Qi.
-    Returns temperatures in K, along with the calculated quasiparticle numbers. 
-    If uselowtempapprox, the complex impedence is calculated directly with a low 
-    temperature approximation, else it\'s calculated with the cinduct function in kidcalc 
-    (slow).''' 
+    Returns temperatures in K, along with the calculated quasiparticle numbers.
+    If uselowtempapprox, the complex impedence is calculated directly with a low
+    temperature approximation, else it\'s calculated with the cinduct function in kidcalc
+    (slow).'''
     ak_ = ak(S21data)
     hw = S21data[:, 5]*2*np.pi*6.582e-4*1e-6
     d = S21data[0, 25]
@@ -330,4 +329,3 @@ def NqpfromQi(S21data,uselowtempapprox=True,lbd0=0.092,kb=86.17,N0=1.72e4,kbTD=3
             D_ = D(kbTeff,N0,kbTc,kbTD)
             Nqp[i] = S21data[0,14]*nqp(kbTeff,D_,N0)
         return kbT/kb,Nqp
-                   
